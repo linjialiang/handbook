a8748933a95797c63a502ff64a6719088c2183e0
linjialiang <linjialiang@163.com>
2022-02-17 23:13:48 +0800

    update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,5 @@
 import { defineConfig, UserConfig } from "vitepress";
-import locales from "./module/base/locale";
 import * as nav from "./module/base/nav";
-import sidebarEn from "./module/base/sidebar/en";
 import sidebarZh from "./module/base/sidebar/zh";
 
 const head: UserConfig["head"] = [
@@ -9,31 +7,26 @@ const head: UserConfig["head"] = [
 ];
 
 export default defineConfig({
+  base: "/",
   lang: "zh-CN",
   title: "学习手册",
   description: "学习手册包括网上收集的以及自己独立撰写的文档",
   head,
   lastUpdated: true,
 
-  locales: locales,
+  markdown: {
+    lineNumbers: true,
+  },
 
   themeConfig: {
-    // Type is `DefaultTheme.Config`
-    locales: {
-      // 简体中文
-      "/": {
-        label: "中文",
-        selectText: "选择语言",
-        nav: [...nav.zh],
-        sidebar: [...sidebarZh],
-      },
-      // English
-      "/en/": {
-        label: "English",
-        selectText: "Languages",
-        nav: [...nav.en],
-        sidebar: [...sidebarEn],
-      },
-    },
+    repo: "linjialiang/handbook",
+    docsDir: "",
+    docsBranch: "main",
+    editLinks: true,
+    editLinkText: "Edit this page on GitHub",
+    lastUpdated: "Last Updated",
+
+    nav: [...nav.zh],
+    sidebar: [...sidebarZh],
   },
 });
