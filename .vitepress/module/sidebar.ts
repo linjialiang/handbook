e3a4421887030ca70758fdc383321fba6bf6dbee
linjialiang <linjialiang@163.com>
2022-02-18 21:16:40 +0800

    update

--- .vitepress/module/sidebar.ts
+++ .vitepress/module/sidebar.ts
@@ -2,10 +2,12 @@ import sidebarHome from "./sidebar/home";
 import sidebarQydocs from "./sidebar/qydocs";
 import qydcosDebian from "./sidebar/qydocs/debian";
 import sidebarWangdocs from "./sidebar/wangdocs";
+import wangdocsBash from "./sidebar/wangdocs/bash";
 
 const sidebar = {
   "/qydocs/debian/": [...qydcosDebian],
   "/qydocs/": [...sidebarQydocs],
+  "/wangdocs/bash/": [...wangdocsBash],
   "/wangdocs/": [...sidebarWangdocs],
   "/": [...sidebarHome],
 };
