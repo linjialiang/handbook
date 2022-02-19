c10265f22b71345c22b72736d39943cb2c50ce84
madnesslin <linjialiang@163.com>
2022-02-19 14:40:05 +0800

    update

---
+++ push.ps1
@@ -0,0 +1,10 @@
+# 脚本所在目录
+$dirParent = Split-Path -Parent $MyInvocation.MyCommand.Definition
+Set-Location $dirParent
+
+git add .
+git commit -m 'update'
+git push
+git push --tags
+git push coding-linjialiang
+git push --tags coding-linjialiang
